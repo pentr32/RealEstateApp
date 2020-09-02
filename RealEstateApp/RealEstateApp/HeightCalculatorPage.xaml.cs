12b0a9d6261675e9330a23d98d9b4242a919708f
Robert Iulian Zaharia <robzaha@hotmail.com>
2020-09-02 14:14:36 +0200

    Compass done. Added barometer.

---
+++ RealEstateApp/RealEstateApp/HeightCalculatorPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using RealEstateApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealEstateApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeightCalculatorPage : ContentPage
+    {
+        #region Properties
+        SensorSpeed speed = SensorSpeed.UI;
+        public double CurrentPressure { get; set; }
+        public double CurrentAltitude { get; set; }
+        BarometerMeasurement BarometerMeas = new BarometerMeasurement();
+        #endregion Properties
+
+        public HeightCalculatorPage()
+        {
+            InitializeComponent();
+            ToggleBarometer();
+        }
+
+        #region LifeTime
+        protected override void OnAppearing()
+        {
+            Barometer.ReadingChanged += Barometer_ReadingChanged;
+        }
+
+        protected override void OnDisappearing()
+        {
+            Barometer.ReadingChanged -= Barometer_ReadingChanged;
+        }
+        #endregion LifeTime
+
+        private void Barometer_ReadingChanged(object sender, BarometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            CurrentPressure = data.PressureInHectopascals;
+            CurrentAltitude = 44307.694 * (1 - Math.Pow(data.PressureInHectopascals / 1013, 0.190284));
+            BindingContext = this;
+        }
+
+        public void ToggleBarometer()
+        {
+            try
+            {
+                if (Barometer.IsMonitoring)
+                    Barometer.Stop();
+                else
+                    Barometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
---
+++ RealEstateApp/RealEstateApp/Models/BarometerMeasurement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Models
+{
+    public class BarometerMeasurement
+    {
+        public double Pressure { get; set; }
+        public double Altitude { get; set; }
+        public string Label { get; set; }
+        public double HeightChange { get; set; }
+
+        public string Display => $"{Label}: {Altitude:N2}m";
+    }
+}
